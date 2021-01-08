7799bc97967b3b0970ed5057c4f96b3b49c9445d
Sertuğ Kaptan <sertuhan00@gmail.com>
2021-01-08 16:31:26 +0200

    AdvisorScreen
    implemented

---
+++ Database/AddAdvisorScreen.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace Database
+{
+    partial class AddAdvisorScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.add_btn = new System.Windows.Forms.Button();
+            this.del_btn = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(224, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(695, 403);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // add_btn
+            // 
+            this.add_btn.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.add_btn.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.add_btn.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.add_btn.Location = new System.Drawing.Point(45, 360);
+            this.add_btn.Name = "add_btn";
+            this.add_btn.Size = new System.Drawing.Size(122, 30);
+            this.add_btn.TabIndex = 8;
+            this.add_btn.Text = "Add";
+            this.add_btn.UseVisualStyleBackColor = true;
+            this.add_btn.Click += new System.EventHandler(this.add_btn_Click);
+            // 
+            // del_btn
+            // 
+            this.del_btn.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.del_btn.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.del_btn.Location = new System.Drawing.Point(45, 396);
+            this.del_btn.Name = "del_btn";
+            this.del_btn.Size = new System.Drawing.Size(122, 30);
+            this.del_btn.TabIndex = 10;
+            this.del_btn.Text = "Delete";
+            this.del_btn.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label3.Location = new System.Drawing.Point(12, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Instructor ID";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 51);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 99);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(176, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(12, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Student ID";
+            // 
+            // AddAdvisorScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(933, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.del_btn);
+            this.Controls.Add(this.add_btn);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "AddAdvisorScreen";
+            this.Text = "AddAvisorScreen";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button add_btn;
+        private System.Windows.Forms.Button del_btn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Database/AddAdvisorScreen.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Database
+{
+    public partial class AddAdvisorScreen : Form
+    {
+        public AddAdvisorScreen()
+        {
+            InitializeComponent();
+            populate();
+            fillCombo("instructor",comboBox1,"ins_id");
+            fillCombo("student",comboBox2, "std_id");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+        public void populate()
+        {
+            DB db = new DB();
+            db.openConnection();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM advisor a ",db.getConnection());
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void add_btn_Click(object sender, EventArgs e)
+        {
+            if(checkEmptyFields())
+            {
+                MessageBox.Show("Please fill all of the empty boxes", "Empty Fields", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                return;
+            }
+            int ins_id = int.Parse(comboBox1.Text);
+            int std_id = int.Parse(comboBox2.Text);
+            DB db = new DB();
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO `advisor`(`ins_id`, `std_id`) VALUES(@ins_id,@std_id)", db.getConnection());
+            cmd.Parameters.Add("@ins_id", MySqlDbType.Int32).Value = ins_id;
+            cmd.Parameters.Add("@std_id", MySqlDbType.Int32).Value = std_id;
+
+            db.openConnection();
+
+            if (!checkEmptyFields())
+            {
+                if(checkStd())
+                {
+                    MessageBox.Show("Student already has an advisor", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    if (cmd.ExecuteNonQuery() == 1)
+                    {
+                        MessageBox.Show("Advisor successfully Assigned!", "Account", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                        MessageBox.Show("Unexpected Error has occured", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill all of the empty boxes", "Empty Fields", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+            populate();
+            db.closeConnection();
+
+        }
+        public Boolean checkStd()
+        {
+            DB db = new DB();
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `advisor` WHERE `std_id` = @usn", db.getConnection());
+            command.Parameters.Add("@usn", MySqlDbType.Int32).Value = int.Parse(comboBox2.Text);
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Boolean checkData(TextBox box,String table_name,String clmn)
+        {
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM "+ table_name + " WHERE "+ clmn +" = "+ int.Parse(box.Text), db.getConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Boolean checkEmptyFields()
+        {
+            String ins_id = comboBox1.Text;
+            String std_id = comboBox2.Text;
+   
+
+            if (ins_id.Equals("") || std_id.Equals(""))
+                return true;
+            else
+                return false;
+
+        }
+        public void fillCombo(String table,ComboBox cb,String str)
+        {
+            try
+            {
+                DB db = new DB();
+                String selectQuery = "SELECT * FROM " + table;
+                db.openConnection();
+                MySqlCommand command = new MySqlCommand(selectQuery, db.getConnection());
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    cb.Items.Add(reader.GetString(str));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
